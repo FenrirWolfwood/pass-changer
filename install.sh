6d79a9545f2076ad33eae46e8ef23a04380ba4b0
FenrirWolfwood <fenrir_wolfwood@protonmail.com>
2022-05-17 16:34:33 +0200

    Ajustes varios y mensajes de terminal.

--- install.sh
+++ install.sh
@@ -3,29 +3,35 @@
 
 #### Script de instalación para "pass-changer": Programa de cambio rápido de claves para el usuario. ####
 
-# Realiza BackUp del fichero de claves /etc/shadow a /etc/shadow.bak por si hay algún problema.
-sudo cp /etc/shadow /etc/shadow.bak
+echo -e ""
+echo -e "Introduzca su contraseña para autorizar las operaciones."
+sudo echo ""
 
 # Comprueba que /opt/pass-changer no exista.
 if [[ -e /opt/pass-changer ]]; then
-    echo "El directorio /opt/pass-changer ya existe. Cancelando la instalación."
+    echo -e " \033[1;31m*\033[0m El directorio /opt/pass-changer ya existe. Cancelando la instalación."
+    echo -e ""
     exit
 fi
+
+# Realiza BackUp del fichero de claves /etc/shadow a /etc/shadow.bak por si hay algún problema.
+echo -e " \033[1;32m*\033[0m Creando copia de /etc/shadow en /etc/shadow.bak como BackUp."
+sudo cp /etc/shadow /etc/shadow.bak
 
 # Copia los ficheros del programa en /opt/pass-changer.
 sudo mkdir -p /opt/pass-changer
 sudo cp -R . /opt/pass-changer
-sudo chmod -R 755 /opt/pass-changer
-echo "El directorio /opt/pass-changer se ha copiado correctamente."
+sudo chmod -R 755 /opt/pass-changer/pass-changer.sh /opt/pass-changer/install.sh /opt/pass-changer/uninstall.sh
+echo -e " \033[1;32m*\033[0m El directorio /opt/pass-changer se ha copiado correctamente."
 
 # Añade a /etc/sudoers la excepción para que no pida clave de SUDO con este script.
 if sudo grep -q "/opt/pass-changer" "/etc/sudoers"; then
-    echo "El registro /opt/pass-changer ya existe en /etc/sudoers."
+    echo -e " \033[1;31m*\033[0m El registro /opt/pass-changer ya existe en /etc/sudoers."
 elif sudo grep -q "PERSONALIZADO" "/etc/sudoers"; then
     echo "" | sudo tee -a /etc/sudoers &> /dev/null
     echo "# Script para cambiar la clave del usuario con atajos de teclado." | sudo tee -a /etc/sudoers &> /dev/null
     echo "%sudo   ALL=NOPASSWD: /opt/pass-changer/pass-changer.sh" | sudo tee -a /etc/sudoers &> /dev/null
-    echo "El registro de /opt/pass-changer se ha añadido en la sección PERSONALIZADO de /etc/sudoers."
+    echo -e " \033[1;32m*\033[0m El registro de /opt/pass-changer se ha añadido a /etc/sudoers en la sección PERSONALIZADO."
 else
     echo "" | sudo tee -a /etc/sudoers &> /dev/null
     echo "" | sudo tee -a /etc/sudoers &> /dev/null
@@ -34,8 +40,10 @@ else
     echo "" | sudo tee -a /etc/sudoers &> /dev/null
     echo "# Script para cambiar la clave del usuario con atajos de teclado." | sudo tee -a /etc/sudoers &> /dev/null
     echo "%sudo   ALL=NOPASSWD: /opt/pass-changer/pass-changer.sh" | sudo tee -a /etc/sudoers &> /dev/null
-    echo "Se ha añadido la sección PERSONALIZADO en /etc/sudoers y se ha añadido en ella el registro de /opt/pass-changer."
+    echo -e " \033[1;32m*\033[0m Se ha añadido la sección PERSONALIZADO en /etc/sudoers y se ha añadido el registro de /opt/pass-changer en ella."
 fi
+
+echo -e ""
 
 
 # Ya solo falta añadir manualmente los atajos de teclado con el siguiente comando:
@@ -43,3 +51,4 @@ fi
 #   sudo /opt/pass-changer/pass-changer.sh USUARIO NUM-CLAVE
 #
 # Donde USUARIO es el nombre de tu usuario y NUM-CLAVE es el numero de la clave elegida.
+
--- uninstall.sh
+++ uninstall.sh
@@ -3,25 +3,39 @@
 
 #### Script de desinstalación para "pass-changer": Programa de cambio rápido de claves para el usuario. ####
 
+echo -e ""
+echo -e "Introduzca su contraseña para autorizar las operaciones."
+sudo echo ""
+
+# Comprueba si existe el BacUp de claves en /etc/shadow.bak y si no está lo crea de nuevo.
+if [[ -e /etc/shadow.bak ]]; then
+    echo -e " \033[1;32m*\033[0m Por precaución se conserva el BackUp de claves en /etc/shadow.bak creado durante la instalación."
+else
+    echo -e " \033[1;33m*\033[0m No se ha encontrado en /etc/shadow.bak ningún BackUp de claves. Se creará uno nuevo por precaución."
+    sudo cp /etc/shadow /etc/shadow.bak
+fi
+
 # Borra el directorio del programa en /opt/pass-changer, pero sin embargo deja el BackUp de /etc/shadow.bak.
 if [[ -e /opt/pass-changer ]]; then
     sudo rm -fr /opt/pass-changer
-    echo "El directorio /opt/pass-changer ha sido borrado."
+    echo -e " \033[1;32m*\033[0m El directorio /opt/pass-changer ha sido borrado."
 else
-    echo "El directorio /opt/pass-changer no existe."
+    echo -e " \033[1;31m*\033[0m El directorio /opt/pass-changer no existe y no ha podido ser borrado."
 fi
 
 # Localiza y borra el registro de /etc/sudoers.
 linea=$(sudo sed -n "/pass-changer/=" /etc/sudoers)
 
 if [[ $linea == "" ]]; then
-    echo "No existe el registro en /etc/sudoers."
+    echo -e " \033[1;31m*\033[0m No se ha encontrado el registro en /etc/sudoers para poder borrarlo."
 else
     linea=$(($linea - 2))
 
     for i in 1 2 3; do
         sudo sed -i "$linea d" /etc/sudoers
     done
+
+    echo -e " \033[1;32m*\033[0m El registro en /etc/sudoers ha sido borrado."
 fi
 
 if [[ $(sudo tail -1 /etc/sudoers | grep -w PERSONALIZADO) != "" ]]; then
@@ -31,6 +45,12 @@ if [[ $(sudo tail -1 /etc/sudoers | grep -w PERSONALIZADO) != "" ]]; then
     for i in 1 2 3 4; do
         sudo sed -i "$linea d" /etc/sudoers
     done
+
+    echo -e " \033[1;32m*\033[0m La sección PERSONALIZADO ha quedado vacía y también ha sido borrada."
 fi
 
+echo -e ""
+
+
 # Ya solo falta eliminar manualmente los atajos de teclado crados para este programa.
+
